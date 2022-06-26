6e054cc2f5d84b891e0183ea93bba5a796f7ba54
Sherreka <rekadarnb@gmail.com>
2022-06-26 14:03:56 -0400

    initialize files

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+// Require express router
+const router = require('express').Router();
+
+// Import all of the API routes 
+const apiRoutes = require('./api');
+
+// add prefix of `/api` to all of the api routes
+router.use('/api', apiRoutes);
+
+// 404 Status error message
+router.use((req, res) => {
+    res.status(404).send('<h1> 😬 404 Error....</h1>');
+  });
+
+// Module exports router
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+// Require express and mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(require('./routes'));
+
+// Connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Log mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
