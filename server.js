ce3159c2f584e609d7cdbd0eeaf20fb3f50159d1
Sherreka <rekadarnb@gmail.com>
2022-06-29 10:26:19 -0400

    removed error in server

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(require('./routes'));
 
 // Connect mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
