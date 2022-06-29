16825e6597c953e753a8acfb04603dddedbb991f
Sherreka <rekadarnb@gmail.com>
2022-06-28 21:23:22 -0400

    added moment

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Thoughts = require('./Thoughts');
 
-module.exports = { User, Thought };
+module.exports = { User, Thoughts };
